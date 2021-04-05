cc82d49b11561c5157386c6ed713a11064b06b7a
Tom Dalling <tom@tomdalling.com>
2021-04-05 15:31:40 +1000

    Remove configurable sidecar path
    Not actually needed right now

--- lib/nodoor/repo.rb
+++ lib/nodoor/repo.rb
@@ -1,15 +1,10 @@
 module Nodoor
   class Repo
-    DEFAULT_SIDECAR_METADATA_EXT = '.nodoor_metadata.yml'
+    SIDECAR_METADATA_EXT = '.nodoor_metadata.yml'
 
-    def initialize(
-      base_directory,
-      file_system: FileSystem.default,
-      sidecar_metadata_ext: DEFAULT_SIDECAR_METADATA_EXT
-    )
+    def initialize(base_directory, file_system: FileSystem.default)
       @base_directory = Pathname.new(base_directory)
       @file_system = file_system
-      @sidecar_metadata_ext = sidecar_metadata_ext
     end
 
     def each
@@ -17,7 +12,7 @@ module Nodoor
 
       file_system.each_file_under(base_directory) do |path|
         next if hidden?(path)
-        next if path.end_with?(sidecar_metadata_ext)
+        next if path.end_with?(SIDECAR_METADATA_EXT)
 
         relative_path = Pathname(path).relative_path_from(base_directory)
         yield fetch(relative_path)
@@ -30,13 +25,13 @@ module Nodoor
 
     def metadata_for(record_path)
       full_path = base_directory / record_path
-      sidecar_path = full_path.sub_ext(full_path.extname + sidecar_metadata_ext)
+      sidecar_path = full_path.sub_ext(full_path.extname + SIDECAR_METADATA_EXT)
       load_sidecar(sidecar_path) || load_frontmatter(full_path)
     end
 
     private
 
-      attr_reader :base_directory, :file_system, :sidecar_metadata_ext
+      attr_reader :base_directory, :file_system
 
       def hidden?(path)
         File.basename(path).start_with?('.')
@@ -48,9 +43,9 @@ module Nodoor
         end
       end
 
-      def load_sidecar(path)
-        return nil unless file_system.exist?(path)
-        YAML.safe_load(file_system.read(path))
+      def load_sidecar(sidecar_path)
+        return nil unless file_system.exist?(sidecar_path)
+        YAML.safe_load(file_system.read(sidecar_path))
       end
   end
 end
